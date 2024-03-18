3db3d7bc66342b9571ace3882c72e0f64b5ebfdd
Benjamin Bordel <benjamin.bordel.64@gmail.com>
2024-03-18 15:41:46 +0100

    chore: init front project, initialize jobForm and jobTable

---
+++ front/src/components/jobForm.tsx
@@ -0,0 +1,11 @@
+interface CreateJobFormProps {
+    onAddJob: () => void;
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const JobForm = (_props: CreateJobFormProps) => {
+
+};
+
+export default JobForm;
---
+++ front/src/components/jobTable.tsx
@@ -0,0 +1,9 @@
+function JobTable() {
+    return (
+        <div>
+        <h2>Job Table</h2>
+        </div>
+    );
+}
+
+export default JobTable;
