309775c029faa63a1c9a0cd662962bd0a9860fbe
Benjamin Bordel <benjamin.bordel.64@gmail.com>
2024-03-20 00:50:43 +0100

    chore(jobservice): put domain <> proto converter on common pkg

---
+++ back/common/domainProtoConverters/domainProtoConverters.go
@@ -0,0 +1,63 @@
+package domainProtoConverters
+
+import (
+	"github.com/BenjaminB64/fullstack-test/back/common/domain"
+	"github.com/BenjaminB64/fullstack-test/back/common/protobuf/jobs-proto"
+)
+
+func DomainJobToGRPCJob(job *domain.Job) *jobs_proto.Job {
+	return &jobs_proto.Job{
+		Id:     int32(job.ID),
+		Type:   DomainTaskTypeToGRPCTaskType(job.TaskType),
+		Status: DomainStatusToGRPCStatus(job.Status),
+		Result: nil,
+	}
+}
+
+func DomainTaskTypeToGRPCTaskType(taskType domain.JobTaskType) jobs_proto.JobType {
+	switch taskType {
+	case domain.JobTaskType_Weather:
+		return jobs_proto.JobType_JOB_TYPE_GET_WEATHER
+	case domain.JobTaskType_GetChabanDelmasBridgeStatus:
+		return jobs_proto.JobType_JOB_TYPE_GET_CHABAN_DELMAS_BRIDGE_STATUS
+	default:
+		return jobs_proto.JobType_JOB_TYPE_UNKNOWN
+	}
+}
+
+func DomainStatusToGRPCStatus(status domain.JobStatus) jobs_proto.JobStatus {
+	switch status {
+	case domain.JobStatus_Pending:
+		return jobs_proto.JobStatus_JOB_STATUS_PENDING
+	case domain.JobStatus_InProgress:
+		return jobs_proto.JobStatus_JOB_STATUS_IN_PROGRESS
+	case domain.JobStatus_Completed:
+		return jobs_proto.JobStatus_JOB_STATUS_COMPLETED
+	case domain.JobStatus_Failed:
+		return jobs_proto.JobStatus_JOB_STATUS_FAILED
+	default:
+		return jobs_proto.JobStatus_JOB_STATUS_UNKNOWN
+	}
+}
+
+func GrpcJobToDomainJob(job *jobs_proto.Job) *domain.Job {
+	return &domain.Job{
+		ID:     int(job.Id),
+		Status: GrpcStatusToDomainStatus(job.Status),
+	}
+}
+
+func GrpcStatusToDomainStatus(status jobs_proto.JobStatus) domain.JobStatus {
+	switch status {
+	case jobs_proto.JobStatus_JOB_STATUS_PENDING:
+		return domain.JobStatus_Pending
+	case jobs_proto.JobStatus_JOB_STATUS_IN_PROGRESS:
+		return domain.JobStatus_InProgress
+	case jobs_proto.JobStatus_JOB_STATUS_COMPLETED:
+		return domain.JobStatus_Completed
+	case jobs_proto.JobStatus_JOB_STATUS_FAILED:
+		return domain.JobStatus_Failed
+	default:
+		return domain.JobStatus_Unknown
+	}
+}
--- back/jobservice/infrastructure/grpc_server/grpc.go
+++ back/jobservice/infrastructure/grpc_server/grpc.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	commonDomain "github.com/BenjaminB64/fullstack-test/back/common/domain"
+	"github.com/BenjaminB64/fullstack-test/back/common/domainProtoConverters"
 	jobsproto "github.com/BenjaminB64/fullstack-test/back/common/protobuf/jobs-proto"
 	"github.com/BenjaminB64/fullstack-test/back/jobservice/domain"
 	"github.com/BenjaminB64/fullstack-test/back/jobservice/infrastructure/config"
@@ -60,14 +60,14 @@ func (s *GrpcServer) Stop() error {
 	return nil
 }
 
-func (s *GrpcServer) GetJobToProcess(ctx context.Context, request *jobsproto.GetPendingJobsRequest) (*jobsproto.GetPendingJobsResponse, error) {
+func (s *GrpcServer) GetPendingJobs(ctx context.Context, request *jobsproto.GetPendingJobsRequest) (*jobsproto.GetPendingJobsResponse, error) {
 	jobs, err := s.jobService.GetJobToProcess(ctx)
 	if err != nil {
 		return nil, err
 	}
 	res := &jobsproto.GetPendingJobsResponse{}
 	for _, job := range jobs {
-		res.Jobs = append(res.Jobs, domainJobToGRPCJob(job))
+		res.Jobs = append(res.Jobs, domainProtoConverters.DomainJobToGRPCJob(job))
 	}
 
 	return res, nil
@@ -79,68 +79,11 @@ func (s *GrpcServer) UpdateJobStatus(ctx context.Context, request *jobsproto.Upd
 		return nil, err
 	}
 
-	job.Status = grpcStatusToDomainStatus(request.Job.Status)
+	job.Status = domainProtoConverters.GrpcStatusToDomainStatus(request.Job.Status)
 	_, err = s.jobService.UpdateJob(ctx, job)
 	if err != nil {
 		return nil, err
 	}
 
 	return &jobsproto.UpdateJobStatusResponse{}, nil
-}
-
-func domainJobToGRPCJob(job *commonDomain.Job) *jobsproto.Job {
-	return &jobsproto.Job{
-		Id:     int32(job.ID),
-		Type:   domainTaskTypeToGRPCTaskType(job.TaskType),
-		Status: domainStatusToGRPCStatus(job.Status),
-		Result: nil,
-	}
-}
-
-func domainTaskTypeToGRPCTaskType(taskType commonDomain.JobTaskType) jobsproto.JobType {
-	switch taskType {
-	case commonDomain.JobTaskType_Weather:
-		return jobsproto.JobType_JOB_TYPE_GET_WEATHER
-	case commonDomain.JobTaskType_GetChabanDelmasBridgeStatus:
-		return jobsproto.JobType_JOB_TYPE_GET_CHABAN_DELMAS_BRIDGE_STATUS
-	default:
-		return jobsproto.JobType_JOB_TYPE_UNKNOWN
-	}
-}
-
-func domainStatusToGRPCStatus(status commonDomain.JobStatus) jobsproto.JobStatus {
-	switch status {
-	case commonDomain.JobStatus_Pending:
-		return jobsproto.JobStatus_JOB_STATUS_PENDING
-	case commonDomain.JobStatus_InProgress:
-		return jobsproto.JobStatus_JOB_STATUS_IN_PROGRESS
-	case commonDomain.JobStatus_Completed:
-		return jobsproto.JobStatus_JOB_STATUS_COMPLETED
-	case commonDomain.JobStatus_Failed:
-		return jobsproto.JobStatus_JOB_STATUS_FAILED
-	default:
-		return jobsproto.JobStatus_JOB_STATUS_UNKNOWN
-	}
-}
-
-func grpcJobToDomainJob(job *jobsproto.Job) *commonDomain.Job {
-	return &commonDomain.Job{
-		ID:     int(job.Id),
-		Status: grpcStatusToDomainStatus(job.Status),
-	}
-}
-
-func grpcStatusToDomainStatus(status jobsproto.JobStatus) commonDomain.JobStatus {
-	switch status {
-	case jobsproto.JobStatus_JOB_STATUS_PENDING:
-		return commonDomain.JobStatus_Pending
-	case jobsproto.JobStatus_JOB_STATUS_IN_PROGRESS:
-		return commonDomain.JobStatus_InProgress
-	case jobsproto.JobStatus_JOB_STATUS_COMPLETED:
-		return commonDomain.JobStatus_Completed
-	case jobsproto.JobStatus_JOB_STATUS_FAILED:
-		return commonDomain.JobStatus_Failed
-	default:
-		return commonDomain.JobStatus_Unknown
-	}
 }
