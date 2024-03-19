9f16b2d0c6494092a7cc434a020a5f8baa06d236
Benjamin Bordel <benjamin.bordel.64@gmail.com>
2024-03-19 11:22:22 +0100

    chore(jobprocessor): init jobprocessor project

---
+++ back/jobprocessor/go.mod
@@ -0,0 +1,32 @@
+module github.com/BenjaminB64/fullstack-test/back/jobprocessor
+
+go 1.22.1
+
+require (
+	github.com/knadh/koanf/providers/env v0.1.0
+	github.com/knadh/koanf/v2 v2.1.0
+	github.com/volatiletech/null v8.0.0+incompatible
+)
+
+require (
+	github.com/friendsofgo/errors v0.9.2 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/volatiletech/inflect v0.0.1 // indirect
+	github.com/volatiletech/sqlboiler v3.7.1+incompatible // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/testify v1.9.0
+	go.uber.org/automaxprocs v1.5.3
+)
---
+++ back/jobprocessor/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"github.com/BenjaminB64/fullstack-test/back/jobprocessor/infrastructure/config"
+	"github.com/BenjaminB64/fullstack-test/back/jobprocessor/infrastructure/logger"
+	"log"
+	"log/slog"
+	"os/signal"
+	"syscall"
+
+	"github.com/BenjaminB64/fullstack-test/back/jobprocessor/application"
+	"go.uber.org/automaxprocs/maxprocs"
+)
+
+func main() {
+	var err error
+
+	ctx, cancelFunc := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancelFunc()
+
+	var c *config.Config
+	c, err = config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var l *logger.Logger
+	l, err = logger.NewLogger(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l.Debug("show config", "config", c)
+
+	_, err = maxprocs.Set(maxprocs.Logger(l.PrintfLogger(slog.LevelDebug)))
+	if err != nil {
+		l.Error("error setting maxprocs", "error", err)
+		return
+	}
+
+	var app *application.Application
+	app, err = application.NewApplication(l, c, ctx)
+	if err != nil {
+		l.Error("error creating application", "error", err)
+		return
+	}
+
+	err = app.Run(ctx)
+	if err != nil {
+		l.Error("error running application", "error", err)
+		return
+	}
+
+	l.Info("application stopped")
+
+}
