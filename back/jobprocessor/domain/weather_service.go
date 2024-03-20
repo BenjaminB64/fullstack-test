a9b7118da5220c9455551be80293fec76252a6d3
Benjamin Bordel <benjamin.bordel.64@gmail.com>
2024-03-20 01:16:06 +0100

    feat(jobprocessor): add weather service

---
+++ back/jobprocessor/domain/weather_service.go
@@ -0,0 +1,12 @@
+package domain
+
+type WeatherService interface {
+	GetWeather() (Weather, error)
+}
+
+// value object
+type Weather struct {
+	Temperature      float64
+	RelativeHumidity float64
+	WeatherWmoCode   int
+}
